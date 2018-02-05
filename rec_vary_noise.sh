400c7eea9040412f8b7af4967eecf7ad77a6b2dc
Alessio Buccino <alejoe9187@gmail.com>
2018-02-05 14:12:32 -0800

    noise mod

--- rec_vary_noise.sh
+++ rec_vary_noise.sh
@@ -2,7 +2,7 @@
 
 if [ $# == 0 ];
   then
-    echo "Supply spike path, ncells, duration, noise ('list')"
+    echo "Supply spike path, ncells, duration, noise ('list'), seed"
 else
     if [ $# == 1 ] && [ "$1" == "all" ];
       then
@@ -12,10 +12,11 @@ else
         ncells=$2
         dur=$3
         noise=$4
+        seed=$5
         for n in $noise
         do
         echo "Generating recording: ncells $ncells duration $dur noise $n"
-        python generate_recordings.py -f $spikes -dur $dur -ncells $ncells -noiselev $n -sync 0 -noplot
+        python generate_recordings.py -f $spikes -dur $dur -ncells $ncells -noiselev $n -sync 0 -noplot -noisemod
         done
         echo All done
     fi
